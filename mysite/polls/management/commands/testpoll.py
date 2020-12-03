5603a8ff303f5a24fe63ccab3c49fbfd75133406
fgsoap <fgsoap@gmail.com>
2020-12-03 20:40:52 +0800

    feat: add management command of testpoll

---
+++ mysite/polls/management/commands/testpoll.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('action', type=str)
+
+    def handle(self, *args, **options):
+        action = options['action']
+        if action == 'raise':
+            raise CommandError('raise error')
+        self.stdout.write(self.style.SUCCESS('testpoll'))
